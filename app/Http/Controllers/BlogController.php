366a337b642545534e9c6902505e54f82abd17c1
CSUNIL1 <schemudupati@gmail.com>
2021-06-24 17:12:43 +0530

    first commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+use App\Http\Controllers\Auth;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $blogs = Blog::all();
+        // dd($titles->title);
+        // foreach($blogs as $blog){
+        //     echo $blog->title;
+        // }
+        return view('index',compact('blogs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        Blog::create(['title'=>$request->title,'content'=>$request->content,'user_id'=>auth()->user()->id]);
+        return redirect('/blogs');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $blog = Blog::findOrFail($id);
+        return view('show',compact('blog'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $blog = Blog::findOrFail($id);
+        // dd($blog);
+        return view('edit',compact('blog'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $blog =Blog::findOrFail($id);
+        $blog->update(['title'=>$request->title,'content'=>$request->content]);
+
+        
+        // $blog->save();
+        // return redirect('/blogs');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+
+        return redirect('/blogs');
+    }
+}
